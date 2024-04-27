828eb56d5042cea88db8219594b338d109a8908d
vmathu <anhthuvominh1@gmail.com>
2024-04-27 08:25:00 +0700

    feat: init map

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
+import './App.css';
+import { APIProvider, Map, Marker } from '@vis.gl/react-google-maps';
+import CircularProgress from '@mui/material/CircularProgress';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const API_KEY = 'AIzaSyAe_FAtgZw3zJZN9RySh-4WMVHzXruyuaA';
+  const [currentLocation, setCurrentLocation] = useState({ lat: 0, lng: 0 });
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      setCurrentLocation({
+        lat: position.coords.latitude,
+        lng: position.coords.longitude,
+      });
+      setLoading(false);
+    });
+  }, []);
+
+  if (loading) {
+    return <CircularProgress />;
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <APIProvider apiKey={API_KEY}>
+      <Map
+        style={{ width: '100vw', height: '100vh' }}
+        defaultCenter={currentLocation}
+        defaultZoom={20}
+        gestureHandling={'greedy'}
+        disableDefaultUI={true}
+      >
+        <Marker position={currentLocation} />
+      </Map>
+    </APIProvider>
+  );
 }
 
-export default App
+export default App;
